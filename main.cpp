294514685c2dd977cbe8cd5944a506d4298197cb
unknown <szymonmatlosz64@gmail.com>
2023-01-31 17:35:59 +0100

    .

--- main.cpp
+++ main.cpp
@@ -13,8 +13,8 @@
 using namespace std;
 int main()
 {
-    char czywyporzycza; //pyta czy urzytkownik wyporzycza samochod
-    char czydodaje; //pyta czy urzytkownik dodaj samochod do wyporzyczalni 
+    char czy_wyporzycza; //pyta czy urzytkownik wyporzycza samochod
+    char czy_dodaje; //pyta czy urzytkownik dodaj samochod do wyporzyczalni 
     char potwierdzenie_dodania;
     string model; //model samochodu
     string kolor; //kolor samochodu
@@ -28,16 +28,32 @@ int main()
         <<endl
         <<endl
         <<"Czy chcialbys wypozyczyc samochod? (T/N)"<<endl;
-    cin >> czywyporzycza;
-    if(czywyporzycza == 'T' || czywyporzycza == 't')
+    cin >> czy_wyporzycza;
+    if(czy_wyporzycza == 'T' || czy_wyporzycza == 't')
     {
-
+        int wielkosc_tablicy;
+        string tab[wielkosc_tablicy];
+        ifstream wczytywanie_bazy;
+        wczytywanie_bazy.open("baza.txt");
+        if(wczytywanie_bazy.is_open())
+        {
+            wczytywanie_bazy >> wielkosc_tablicy;
+            for(int i=0;i<wielkosc_tablicy;i++)
+            {
+                wczytywanie_bazy >> tab[i];
+            }
+        }
+        wczytywanie_bazy.close();
+        for(int i=0; i<wielkosc_tablicy; i++)
+        {
+            cout << tab[i];
+        }
     }
     else
     {
         cout << "Czy chcialbys dodac samochod do wypozyczalni? (T/N)"<<endl;
-        cin >> czydodaje;
-        if(czydodaje == 'T' || czydodaje == 't')
+        cin >> czy_dodaje;
+        if(czy_dodaje == 'T' || czy_dodaje == 't')
         {
             cout <<"Podaj informacje dla samochodu ktorego chcialbys wystawic do naszej wyporzyczalni"
                 <<endl;
@@ -45,16 +61,23 @@ int main()
             cin>> firma;
             cout<<"Model: ";
             cin >> model;
+            cout << "Rocznik: ";
+
+            cout << "Przebieg: ";
+
             cout <<"Kolor: ";
             cin >> kolor;
             cout <<endl<<"Dodaj samochod? (T/N)" <<endl;
             cin >> potwierdzenie_dodania;
             if(potwierdzenie_dodania == 'T' || potwierdzenie_dodania == 't')
             {
-                fstream plik;
-                plik.open("plik.txt");
-                    plik <<"Firma: "<< firma << " || Model: " << model <<" || Kolor: " << kolor;
-                plik.close();
+                ofstream dodaj;
+                dodaj.open("baza.txt", ios::app);
+                if(dodaj.is_open())
+                {
+                    dodaj <<"Firma: "<< firma << " || Model: " << model <<" || Kolor: " << kolor <<endl;
+                }
+                dodaj.close();
             }
             
         } 
