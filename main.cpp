6a931c799bf6893d81a0c39fc4f6166452da317f
unknown <szymonmatlosz64@gmail.com>
2023-01-31 10:58:46 +0100

    Dodano dodawanie samochodow do pliku txt

--- main.cpp
+++ main.cpp
@@ -1,38 +1,66 @@
+/**
+ * @file main.cpp
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-01-31
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
 #include <iostream> 
 #include <fstream>
 using namespace std;
 int main()
 {
-    char czywyporzycza;
-    char czydodaje;
-    cout <<"+-----------------------------+"
+    char czywyporzycza; //pyta czy urzytkownik wyporzycza samochod
+    char czydodaje; //pyta czy urzytkownik dodaj samochod do wyporzyczalni 
+    char potwierdzenie_dodania;
+    string model; //model samochodu
+    string kolor; //kolor samochodu
+    string firma;
+
+    cout <<"+-------------------------------------------+"
         <<endl
-        <<"| Witam w naszej wypozyczalni |"
+        <<"| Witam w naszej  internetowej wypozyczalni! |"
         <<endl
-        <<"+-----------------------------+"
+        <<"+-------------------------------------------+"
         <<endl
         <<endl
-        <<"Czy chcialbys wypozyczyc samochod? (Y/N)"<<endl;
+        <<"Czy chcialbys wypozyczyc samochod? (T/N)"<<endl;
     cin >> czywyporzycza;
-    if(czywyporzycza == 'Y')
+    if(czywyporzycza == 'T' || czywyporzycza == 't')
     {
 
     }
     else
     {
-        cout << "Czy chcialbys dodac samochod do wypozyczalni? (Y/N)"<<endl;
+        cout << "Czy chcialbys dodac samochod do wypozyczalni? (T/N)"<<endl;
         cin >> czydodaje;
-        if(czydodaje == 'Y')
+        if(czydodaje == 'T' || czydodaje == 't')
         {
-
+            cout <<"Podaj informacje dla samochodu ktorego chcialbys wystawic do naszej wyporzyczalni"
+                <<endl;
+            cout<<"Firma: ";
+            cin>> firma;
+            cout<<"Model: ";
+            cin >> model;
+            cout <<"Kolor: ";
+            cin >> kolor;
+            cout <<endl<<"Dodaj samochod? (T/N)" <<endl;
+            cin >> potwierdzenie_dodania;
+            if(potwierdzenie_dodania == 'T' || potwierdzenie_dodania == 't')
+            {
+                fstream plik;
+                plik.open("plik.txt");
+                    plik <<"Firma: "<< firma << " || Model: " << model <<" || Kolor: " << kolor;
+                plik.close();
+            }
+            
         } 
         else
         {
             return 0;
         }
     }
-    fstream plik;
-    plik.open("plik.txt");
-    
-    plik.close();
 }
